e77fefe5725af1ced3f86479f8779aab44b9e6dc
htsteelp1 <htsteelp1@gmail.com>
2026-09-17 11:21:43 -0400

    add frontend function for getting servers by id

---
+++ client/src/api/servers.ts
@@ -0,0 +1,12 @@
+export async function getServerById(id) {
+    try {
+        const res = await fetch("/api/servers/:id");
+        if (!res.ok) {
+            throw new Error(`Failed to fetch server: ${res.status}`)
+        }
+        return await res.json();
+    }
+    catch (e) {
+        console.error(e);
+    }
+}
