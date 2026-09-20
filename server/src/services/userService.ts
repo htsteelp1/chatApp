f9cfdc85f886aaeadc7a61cf92dd4532b1013523
htsteelp1 <htsteelp1@gmail.com>
2026-09-20 15:26:30 -0400

    fix bug where req.user was used instead of new args

--- server/src/services/userService.ts
+++ server/src/services/userService.ts
@@ -2,10 +2,10 @@ import bcrypt from "bcrypt";
 import {db} from "../prisma/db";
 
 export async function createUser(name: String, password: String) {
-    const hash = await bcrypt.hash(req.body.password, 10);
-    return await db.orm.public.User.create({name: req.body.username, hash: hash});
+    const hash = await bcrypt.hash(password, 10);
+    return await db.orm.public.User.create({name, hash});
 }
 export async function getMemberships(id: String) {
-    const user = await db.orm.public.User.where({id: req.user.id}).include("servers").first();
+    const user = await db.orm.public.User.where({id}).include("servers").first();
     return user?.servers ?? [];
 }
